6983281108cb182fa6ec71a44516426ce9cbf753
LOURENCO ANDREA <3502335@ppti-24-301-15.ufr-info-p6.jussieu.fr>
2017-04-28 13:20:54 +0200

    feat(front): init first views

---
+++ Stalker/Stalker/ConnexionController.swift
@@ -0,0 +1,25 @@
+//
+//  ConnexionController.swift
+//  Stalker
+//
+//  Created by m2sar on 27/04/17.
+//  Copyright © 2017 m2sar. All rights reserved.
+//
+
+import UIKit
+
+class ConnexionController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setu p after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Stalker/Stalker/MenuController.swift
@@ -0,0 +1,25 @@
+//
+//  MenuController.swift
+//  Stalker
+//
+//  Created by m2sar on 27/04/17.
+//  Copyright © 2017 m2sar. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MenuController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
