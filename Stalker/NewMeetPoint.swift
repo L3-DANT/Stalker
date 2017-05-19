19ed21ff83b59519269078fcc45c2f9840b1206a
loatR <m1105.111.dell@gmail.com>
2017-05-19 18:32:26 +0200

    feat(views): changes views

---
+++ Stalker/NewMeetPoint.swift
@@ -0,0 +1,13 @@
+//
+//  NewMeetPoint.swift
+//  Stalker
+//
+//  Created by m2sar on 10/05/17.
+//  Copyright © 2017 m2sar. All rights reserved.
+//
+
+import Foundation
+
+class NewMeetPoint {
+    
+}
