e0cb2f9f828ce1bdbdf5e2ad3254d3c7712fc641
alan-camilo <alan.c.herrera@gmail.com>
2017-05-19 20:29:27 +0200

    feature(request login): work in progress

--- Stalker/ConnexionController.swift
+++ Stalker/ConnexionController.swift
@@ -13,8 +13,59 @@ class ConnexionController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var emailInput: UITextField!
     @IBOutlet weak var passwordInput: UITextField!
     
-    let session = Session()
+    var token:String?
     
+    @IBAction func loginButton(_ sender: UIButton) {
+        if (emailInput.text != nil && passwordInput.text != nil) {
+            
+            let formParams : [String: String] = ["mail": emailInput.text!,"password": passwordInput.text!]
+            
+            // create post request
+            let url = URL(string: "http://35.187.15.102:8080/api/user/authenticate")!
+            
+            var request = URLRequest(url: url)
+            
+            request.httpMethod = "POST"
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            
+            // insert json data to the request
+            do {
+                request.httpBody = try JSONSerialization.data(withJSONObject: formParams)
+            }
+            catch {
+                print(error)
+            }
+            
+            let task = URLSession.shared.dataTask(with: request) {
+                data, response, error in
+                
+                // Check for error
+                if (error != nil) {
+                    print("error: \(error)")
+                    return
+                }
+                
+                // Print out response string
+                let responseString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+                print("responseString: \(responseString)")
+                
+                
+//                guard let data = data, error == nil else {
+//                    print(error?.localizedDescription ?? "No data")
+//                    return
+//                }
+                
+//                if let responseJSON = try? JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? NSDictionary {
+//                    self.token = responseJSON?.value(forKeyPath: "token") as? String
+//                    print(self.token)
+//                }
+                
+            }
+            
+            task.resume()
+            
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
